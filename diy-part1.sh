7eee082be8caf76b0b3b5eb4c8e22dc7d8c4cdb3
wekingchen <weking.chen@gmail.com>
2021-08-28 22:05:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-wget 'https://raw.githubusercontent.com/wekingchen/my-file/master/Actions-OpenWrt.zip' --no-check-certificate && sudo unzip Actions-OpenWrt.zip && sudo rm -f Actions-OpenWrt.zip
+wget 'https://raw.githubusercontent.com/wekingchen/my-file/master/Actions-OpenWrt.zip' --no-check-certificate && sudo unzip -o Actions-OpenWrt.zip && sudo rm -f Actions-OpenWrt.zip
