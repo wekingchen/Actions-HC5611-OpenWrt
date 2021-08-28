fa4d0272c6b496d1ce4bf6f212e16ab66e6495e8
wekingchen <weking.chen@gmail.com>
2021-08-28 22:14:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,9 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# 解决helloworld源码缺失
 wget 'https://raw.githubusercontent.com/wekingchen/my-file/master/Actions-OpenWrt.zip' --no-check-certificate && sudo unzip -o Actions-OpenWrt.zip && sudo rm -f Actions-OpenWrt.zip
+
+# 解决shadowsocksr-libev源缺少依赖问题
+cd ~/openwrt/package/feeds/helloworld
+ln -s ../../../feeds/helloworld/shadowsocksr-libev/ shadowsocksr-libev
