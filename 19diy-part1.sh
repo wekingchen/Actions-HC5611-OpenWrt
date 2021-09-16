84e4230a33f31c72ff90993f5e393cea07b67005
wekingchen <weking.chen@gmail.com>
2021-09-16 13:04:00 +0800

    Update 19diy-part1.sh

--- 19diy-part1.sh
+++ 19diy-part1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# File name: 19diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
