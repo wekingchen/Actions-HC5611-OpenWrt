4c7592dd545f56cb657e5d76059da31d39abdc5a
wekingchen <weking.chen@gmail.com>
2023-11-07 11:07:57 +0800

    Update diy-part1-passwall.sh

--- diy-part1-passwall.sh
+++ diy-part1-passwall.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo "src-git helloworld https://github.com/fw876/helloworld.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
