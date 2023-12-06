8f724f6de151e9786ad596ec8d090fa434e03824
wekingchen <weking.chen@gmail.com>
2023-12-06 11:39:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-svn co https://github.com/coolsnowwolf/lede/trunk/tools/ninja tools/ninja
+git clone https://github.com/coolsnowwolf/lede.git
+cp -r lede/tools/ninja tools/ninja
+rm -rf lede
