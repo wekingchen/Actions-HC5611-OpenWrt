23c18ede036b8e8511bde120526be22c850aac56
wekingchen <weking.chen@gmail.com>
2024-01-30 11:04:09 +0800

    Update diy-leanssr-part2.sh

--- diy-leanssr-part2.sh
+++ diy-leanssr-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+wget https://codeload.github.com/fw876/helloworld/zip/28504024db649b7542347771704abc33c3b1ddc8 -O helloworld.zip
+unzip helloworld.zip
+rm -rf feeds/helloworld/shadowsocks-rust
+cp -r helloworld-28504024db649b7542347771704abc33c3b1ddc8/shadowsocks-rust feeds/helloworld
+rm -rf helloworld.zip helloworld-28504024db649b7542347771704abc33c3b1ddc8
